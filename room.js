bf2ec030efeeea325be44493b2e837bcc0ba5482
MTTHANH-VD <thanhro456vncm@gmail.com>
2025-11-27 16:50:33 +0700

    update url

--- room.js
+++ room.js
@@ -3,7 +3,7 @@ const params = new URLSearchParams(window.location.search);
 const myName = params.get("name") || "Người dùng";
 document.getElementById("usernameDisplay").innerText = myName;
 
-const serverUrl = "ws://localhost:3000";
+const serverUrl = "wss://your-worker-name.username.workers.dev";
 const socket = new WebSocket(serverUrl);
 
 const videoGrid = document.getElementById("videoGrid");
